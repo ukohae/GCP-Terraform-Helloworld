c56cf5e3c94a037a7c95bb62821e0ce35c3dc63e
developer <ukohae39@gmail.com>
2023-07-21 13:13:55 -0400

    jenkins

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,7 @@ project_id="gcp-terraform-env"
 npm i --silent
 gcloud auth activate-service-account gcp-test@gcp-terraform-env.iam.gserviceaccount.com --key-file=./creds/serviceaccount.json
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io
-docker build . -t gcr.io/${project_id}/app
-docker push gcr.io/${project_id}/app
-docker rmi gcr.io/${project_id}/app
+echo $USER
+sudo docker build . -t gcr.io/${project_id}/app
+sudo docker push gcr.io/${project_id}/app
+sudo docker rmi gcr.io/${project_id}/app
