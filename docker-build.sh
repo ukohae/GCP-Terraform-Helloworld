539b10576429c75504a6bbdc55e8865d3b2a687d
developer <ukohae39@gmail.com>
2023-07-20 21:55:17 -0400

    sudo

--- docker-build.sh
+++ docker-build.sh
@@ -3,8 +3,6 @@
 project_id="gcp-terraform-env"
 npm i --silent
 
-echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sudo sh -c 'echo "$(logname) ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/$(logname)' && sudo chmod 440 /etc/sudoers.d/$(logname)
 sudo docker build . -t gcr.io/${project_id}/app
 gcloud auth activate-service-account gcp-test@gcp-terraform-env.iam.gserviceaccount.com --key-file=./creds/serviceaccount.json
 gcloud auth print-access-token | sudo docker login -u oauth2accesstoken --password-stdin https://gcr.io
