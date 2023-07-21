d357d310c76c1c87ede244abc23ca28a724a663e
developer <ukohae39@gmail.com>
2023-07-20 21:20:50 -0400

    sudo

--- docker-build.sh
+++ docker-build.sh
@@ -2,10 +2,10 @@
 
 project_id="gcp-terraform-env"
 npm i --silent
-sudo -s
-sudo docker build . -t gcr.io/${project_id}/app
+
+docker build . -t gcr.io/${project_id}/app
 gcloud auth activate-service-account gcp-test@gcp-terraform-env.iam.gserviceaccount.com --key-file=./creds/serviceaccount.json
 gcloud auth print-access-token | sudo docker login -u oauth2accesstoken --password-stdin https://gcr.io
-sudo docker push gcr.io/${project_id}/app
-sudo docker rmi gcr.io/${project_id}/app
+docker push gcr.io/${project_id}/app
+docker rmi gcr.io/${project_id}/app
 
