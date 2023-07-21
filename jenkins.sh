2a0cfb838e9e5a4750e28f487889681f3ab329c7
developer <ukohae39@gmail.com>
2023-07-20 22:25:58 -0400

    docker permission

--- jenkins.sh
+++ jenkins.sh
@@ -51,3 +51,6 @@ sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
+sudo usermod -a -G docker jenkins
+sudo usermod -a -G docker $USER
+
